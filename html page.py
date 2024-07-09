95bd2194cbeead6df2b310b3cb4970ba058d52dc
Chetan Lande <160496624+Chetan-Lande@users.noreply.github.com>
2024-07-09 23:12:24 +0530

    Add files via upload

---
+++ html page.py
@@ -0,0 +1,15 @@
+a = open("aaa.html","w")
+a.write(
+    "<html>"
+    "<head>"
+    "<title>snackvb</title>"
+    "</head>"
+    "<body>"
+    "<header>"
+    "<h1 style='font-family: Arial; color: blue;'>"
+    "Horn Oglig will Armock</h1>"
+    "</header>"
+    "</body>"
+    "</html>"
+    )
+a.close()
